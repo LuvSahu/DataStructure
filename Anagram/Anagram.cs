cd352359acf548412093fc9596828c9af6665ef6
Luv sahu <lksahu404@gmail.com>
2022-06-28 09:29:39 +0530

    [Luv SAHU] This code for Anagram

---
+++ Anagram/Anagram.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    internal class Anagram
+    {
+        static int NO_OF_CHARS = 256;
+
+        
+        public static bool areAnagram(string str1,
+                               string str2)
+        {
+
+           
+            if (str1.Length != str2.Length)
+                return false;
+
+
+            Array.Sort(str1.ToCharArray());
+            Array.Sort(str2.ToCharArray());
+            string a = str1.ToString();
+            string b = str2.ToString();
+            if (a != b)
+            {
+
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
+
