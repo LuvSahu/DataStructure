7e306f7efea993d1ca8a21aaf4a1e37ac3661ad7
Luv sahu <lksahu404@gmail.com>
2022-06-28 10:06:06 +0530

    [Luv SAHU] This code for PrimePalindrome

---
+++ PrimePalindrome/PrimePalindromecs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    internal class PrimePalindromecs
+    {
+        public static bool isPalindrome(int n)
+        {
+
+            int rev = 0;
+            for (int i = n; i > 0; i /= 10)
+            {
+                rev = rev * 10 + i % 10;
+            }
+
+            return (n == rev) ? true : false;
+        }
+
+        
+        public static void countPal(int min,
+                                    int max)
+        {
+
+            for (int i = min; i <= max; i++)
+            {
+                if (PrimeNumberRange.IsPrime(i) && isPalindrome(i))
+                {
+                    Console.WriteLine(i);
+                }
+
+                
+
+
+
+            }
+
+
+        }
+    }
+}
